19ac31ad644ad3573080813c0bf5a4bb84d55e41
Sayalee Rajesh Pote <sapote@deloitte.com>
2020-04-16 08:52:44 +0530

    Modified dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -6,15 +6,8 @@ let danger = Danger()
 // but it definitely feels magical.
 SwiftLint.lint(inline: true)
 
-// Have different runs of SwiftLint against different sub-folders
-SwiftLint.lint(directory: "Sources", configFile: ".swiftlint.yml")
-SwiftLint.lint(directory: "Tests", configFile: "Tests/HarveyTests/.swiftlint.yml")
-
 // The equivalent to running `swiftlint` in the root of the folder
 SwiftLint.lint(lintAllFiles: true)
-
-// Use a different path for SwiftLint
-SwiftLint.lint(swiftlintPath: "Pods/SwiftLint/swiftlint")
 
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
